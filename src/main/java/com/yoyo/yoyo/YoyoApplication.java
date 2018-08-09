5d8382775b7426d8d0d627d99bcf12416b143d81
cucyoyo <813393834@qq.com>
2018-08-09 16:22:33 +0800

    初始化

---
+++ src/main/java/com/yoyo/yoyo/YoyoApplication.java
@@ -0,0 +1,12 @@
+package com.yoyo.yoyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoyoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoyoApplication.class, args);
+    }
+}
